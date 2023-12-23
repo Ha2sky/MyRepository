4e21ab7251096a694282ff6f36100ddd29bc9cef
Ha2sky <qkrwhdrjs13@gmail.com>
2023-12-23 15:06:22 +0900

    First commit in devC

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -2,6 +2,8 @@ package mygit;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("A");
+		System.out.println("A_Fix in DevC");
+		System.out.println("This is the 2nd version.");
+		System.out.println("Hello, C");
 	}
 }

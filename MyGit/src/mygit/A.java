352b43e0948cacae7b66ab54654890a118cb8612
Ha2sky <qkrwhdrjs13@gmail.com>
2023-12-23 15:01:04 +0900

    First commit in devB

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -2,6 +2,7 @@ package mygit;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("A");
+		System.out.println("A_Fix in devB");
+		System.out.println("This is the 2nd version.");
 	}
 }
--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -2,6 +2,7 @@ package mygit;
 
 public class B {
 	public static void main(String[] args) {
-		System.out.println("B");
+		System.out.println("B_Fix in devB");
+		System.out.println("This is the 2nd version.");
 	}
 }
